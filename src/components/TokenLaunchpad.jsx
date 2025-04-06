9310d43981def0b0b228813e9e0c76eb4342cc07
Prasad-D-Ware <prasadware42116@gmail.com>
2025-04-06 22:12:29 +0530

    ft : createMint function

--- src/components/TokenLaunchpad.jsx
+++ src/components/TokenLaunchpad.jsx
@@ -1,10 +1,35 @@
 import { WalletDisconnectButton, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { createInitializeMint2Instruction, createMint, getMinimumBalanceForRentExemptMint, MINT_SIZE, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 
 export function TokenLaunchpad() {
-
+    const { connection } = useConnection();
+    const wallet = useWallet();
 
     const createToken = async () =>{
-      
+        const lamports = await getMinimumBalanceForRentExemptMint(connection);
+        const mintAccount = Keypair.generate();
+
+        const transaction = new Transaction().add(
+            SystemProgram.createAccount({
+                fromPubkey: wallet.publicKey,
+                newAccountPubkey: mintAccount.publicKey,
+                space: MINT_SIZE,
+                lamports,
+                programId : TOKEN_PROGRAM_ID,
+            }),
+            createInitializeMint2Instruction(mintAccount.publicKey, 5, wallet.publicKey, wallet.publicKey, TOKEN_PROGRAM_ID),
+        );
+        
+        const latestBlockhash = await connection.getLatestBlockhash();
+        transaction.recentBlockhash = latestBlockhash.blockhash;
+        transaction.feePayer = wallet.publicKey;
+        
+        transaction.partialSign(mintAccount);
+
+        await wallet.sendTransaction(transaction , connection);
+        alert("Transaction Complete")
     }
 
     return  <div style={{
