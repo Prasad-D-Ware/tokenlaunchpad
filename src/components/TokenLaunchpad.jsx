c9ce18565f7fbd9e07f748c6fb7d11e4315250d1
Prasad-D-Ware <prasadware42116@gmail.com>
2025-04-20 09:29:22 +0530

    ft : token 2022 metadata mint creation

--- src/components/TokenLaunchpad.jsx
+++ src/components/TokenLaunchpad.jsx
@@ -1,25 +1,57 @@
 import { WalletDisconnectButton, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
-import { createInitializeMint2Instruction, createMint, getMinimumBalanceForRentExemptMint, MINT_SIZE, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { ASSOCIATED_TOKEN_PROGRAM_ID, createAssociatedTokenAccount, createInitializeInstruction, createInitializeMetadataPointerInstruction, createInitializeMint2Instruction, createInitializeMintInstruction, createMint, ExtensionType, getMinimumBalanceForRentExemptMint, getMintLen, LENGTH_SIZE, MINT_SIZE, TOKEN_2022_PROGRAM_ID, TOKEN_PROGRAM_ID, TYPE_SIZE } from "@solana/spl-token";
 import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { useState } from "react";
+import { pack } from "@solana/spl-token-metadata";
 
 export function TokenLaunchpad() {
+    const [name,setName] = useState("");
+    const [symbol,setSymbol] = useState("");
+    const [imageUrl,setImageUrl] = useState("");
+    const [supply,setSupply] = useState("");
+    const [decimals,setDecimals] = useState(9);
     const { connection } = useConnection();
     const wallet = useWallet();
 
     const createToken = async () =>{
-        const lamports = await getMinimumBalanceForRentExemptMint(connection);
+        console.log(name , symbol , imageUrl , decimals, supply);
         const mintAccount = Keypair.generate();
+
+        const metaData = {
+            mint : mintAccount.publicKey,
+            name,
+            symbol,
+            uri : imageUrl,
+            additionalMetadata: [],
+        }
+
+        const mintLen = getMintLen([ExtensionType.MetadataPointer]);
+        const metadataLen = TYPE_SIZE + LENGTH_SIZE + pack(metaData).length;
+
+        const lamports = await connection.getMinimumBalanceForRentExemption(mintLen + metadataLen);
 
         const transaction = new Transaction().add(
             SystemProgram.createAccount({
                 fromPubkey: wallet.publicKey,
                 newAccountPubkey: mintAccount.publicKey,
-                space: MINT_SIZE,
+                space: mintLen,
                 lamports,
-                programId : TOKEN_PROGRAM_ID,
+                programId : TOKEN_2022_PROGRAM_ID,
             }),
-            createInitializeMint2Instruction(mintAccount.publicKey, 5, wallet.publicKey, wallet.publicKey, TOKEN_PROGRAM_ID),
+            // createInitializeMint2Instruction(mintAccount.publicKey, 5, wallet.publicKey, wallet.publicKey, TOKEN_PROGRAM_ID),
+            createInitializeMetadataPointerInstruction( mintAccount.publicKey,  wallet.publicKey , mintAccount.publicKey , TOKEN_2022_PROGRAM_ID),
+            createInitializeMintInstruction( mintAccount.publicKey ,decimals , wallet.publicKey ,wallet.publicKey , TOKEN_2022_PROGRAM_ID),
+            createInitializeInstruction({
+                programId : TOKEN_2022_PROGRAM_ID,
+                mint : mintAccount.publicKey,
+                metadata : mintAccount.publicKey,
+                name : metaData.name,
+                symbol : metaData.symbol,
+                uri : metaData.uri,
+                updateAuthority : wallet.publicKey,
+                mintAuthority : wallet.publicKey
+            })
         );
         
         const latestBlockhash = await connection.getLatestBlockhash();
@@ -29,7 +61,7 @@ export function TokenLaunchpad() {
         transaction.partialSign(mintAccount);
 
         await wallet.sendTransaction(transaction , connection);
-        alert("Transaction Complete")
+        alert(`Transaction Complete Mint Account : ${mintAccount.publicKey}`);
     }
 
     return  <div style={{
@@ -48,10 +80,11 @@ export function TokenLaunchpad() {
             <WalletDisconnectButton />
        </div>
         <h1>Solana Token Launchpad</h1>
-        <input className='inputText' type='text' placeholder='Name'></input> <br />
-        <input className='inputText' type='text' placeholder='Symbol'></input> <br />
-        <input className='inputText' type='text' placeholder='Image URL'></input> <br />
-        <input className='inputText' type='text' placeholder='Initial Supply'></input> <br />
+        <input className='inputText' type='text' placeholder='Name' onChange={(e)=>setName(e.target.value)}></input> <br />
+        <input className='inputText' type='text' placeholder='Symbol' onChange={(e)=>setSymbol(e.target.value)}></input> <br />
+        <input className='inputText' type='text' placeholder='Image URL' onChange={(e)=>setImageUrl(e.target.value)}></input> <br />
+        <input className='inputText' type='text' placeholder='Decimals' onChange={(e)=>setDecimals(e.target.value)}></input> <br />
+        <input className='inputText' type='text' placeholder='Initial Supply' onChange={(e)=>setSupply(e.target.value)}></input> <br />
         <button className='btn' onClick={createToken}>Create a token</button>
     </div>
 }
