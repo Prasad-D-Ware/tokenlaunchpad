c9f04c111b61315de399f91d97f234a358732448
Prasad-D-Ware <prasadware42116@gmail.com>
2025-04-29 00:14:18 +0530

    ft : unstaking functionality

--- src/components/StakeSol.jsx
+++ src/components/StakeSol.jsx
@@ -1,5 +1,6 @@
 import {
 	ASSOCIATED_TOKEN_PROGRAM_ID,
+	createBurnInstruction,
 	getOrCreateAssociatedTokenAccount,
 	mintTo,
 	TOKEN_2022_PROGRAM_ID,
@@ -13,15 +14,21 @@ import {
 	Keypair,
 	LAMPORTS_PER_SOL,
 	PublicKey,
+	sendAndConfirmTransaction,
 	SystemProgram,
 	Transaction,
 } from "@solana/web3.js";
 import React, { useState } from "react";
 import bs58 from "bs58";
 
+const STAKING_ADDRESS = import.meta.env.VITE_STAKING_ADDRESS;
+const MINT_ADDRESS = import.meta.env.VITE_MINT_ADDRESS;
+
 const Stake = () => {
 	const [amount, setAmount] = useState(0);
+	const [unStakeAmount, setUnstakeAmount] = useState(0);
 	const [lst, setLst] = useState();
+	const [unstakeSol, setUnstakeSol] = useState();
 
 	const wallet = useWallet();
 
@@ -31,6 +38,10 @@ const Stake = () => {
 
 	const setLSTAmount = (amount) => {
 		setTimeout(() => setLst(amount), 3000);
+	};
+
+	const setSolAmount = (amount) => {
+		setTimeout(() => setUnstakeSol(amount), 3000);
 	};
 
 	function base58ToKeypair(base58PrivateKey) {
@@ -46,8 +57,6 @@ const Stake = () => {
 	const stakeSol = async () => {
 		try {
 			// console.log("Staking sol", amount);
-			const STAKING_ADDRESS = import.meta.env.VITE_STAKING_ADDRESS;
-			const MINT_ADDRESS = import.meta.env.VITE_MINT_ADDRESS;
 
 			if (!wallet.publicKey) {
 				alert("Please connect your wallet first");
@@ -70,7 +79,7 @@ const Stake = () => {
 			// console.log("before ata")
 			const associatedTokenAccount = await getOrCreateAssociatedTokenAccount(
 				connection,
-				wallet.publicKey,
+				payer,
 				new PublicKey(MINT_ADDRESS),
 				wallet.publicKey,
 				true,
@@ -103,7 +112,7 @@ const Stake = () => {
 
 			await wallet.sendTransaction(transaction, connection);
 
-			alert("Staked Solana");
+			alert("Staked Solana and sent Berry!");
 
 			await mintTo(
 				connection,
@@ -121,81 +130,183 @@ const Stake = () => {
 		}
 	};
 
+	const withdrawSol = async () => {
+		try {
+			if (!wallet.publicKey) {
+				alert("Please connect your wallet first");
+				return;
+			}
+
+			if (!unStakeAmount) {
+				alert("Please enter a valid unstaking amount");
+				return;
+			}
+
+			const decimals = 9;
+			const associatedTokenAccount = await getOrCreateAssociatedTokenAccount(
+				connection,
+				payer,
+				new PublicKey(MINT_ADDRESS),
+				wallet.publicKey,
+				true,
+				null,
+				null,
+				TOKEN_2022_PROGRAM_ID,
+				ASSOCIATED_TOKEN_PROGRAM_ID
+			);
+
+			const burnTx = new Transaction().add(
+				createBurnInstruction(
+					associatedTokenAccount.address,
+					new PublicKey(MINT_ADDRESS),
+					wallet.publicKey,
+					unStakeAmount * 10 ** decimals,
+					[],
+					TOKEN_2022_PROGRAM_ID
+				)
+			);
+			burnTx.feePayer = wallet.publicKey;
+			burnTx.recentBlockhash = (
+				await connection.getLatestBlockhash()
+			).blockhash;
+			await wallet.sendTransaction(burnTx, connection);
+
+			const solTx = new Transaction().add(
+				SystemProgram.transfer({
+					fromPubkey: payer.publicKey,
+					toPubkey: wallet.publicKey,
+					lamports: unStakeAmount * LAMPORTS_PER_SOL,
+				})
+			);
+			solTx.feePayer = payer.publicKey;
+			solTx.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+			await sendAndConfirmTransaction(connection, solTx, [payer]);
+
+			alert("Unstaked and sent SOL!");
+		} catch (error) {
+			console.error("Error while unstaking:", error);
+			alert("Error while unstaking: " + error.message);
+		}
+	};
+
 	return (
-		<div
-			style={{
-				height: "100vh",
-				display: "flex",
-				justifyContent: "center",
-				alignItems: "center",
-				flexDirection: "column",
-			}}
-		>
+		<>
 			<div
 				style={{
+					height: "100vh",
 					display: "flex",
-					justifyContent: "space-between",
-					gap: "40px",
+					justifyContent: "center",
+					alignItems: "center",
+					flexDirection: "column",
 				}}
 			>
-				<WalletMultiButton />
-				<WalletDisconnectButton />
-			</div>
-			<h1>Liquid Staked Solana </h1>
-			<div
-				style={{
-					display: "flex",
-					gap: "5px",
-				}}
-			>
-				<input
-					className="inputText"
-					type="text"
-					placeholder="Amount"
-					onChange={(e) => {
-						setAmount(Number(e.target.value));
-						setLSTAmount(e.target.value);
-					}}
-				></input>{" "}
-				<img
-					src="https://img.icons8.com/?size=100&id=icTiMgoOHSVy&format=png&color=000000"
-					alt="solana"
-					height={60}
-					width={60}
-				/>
-			</div>
-			<br />
-			<div
-				style={{
-					display: "flex",
-					gap: "5px",
-				}}
-			>
-				<input
-					className="inputText"
-					type="text"
-					placeholder="ZoroBerry"
-					value={lst}
-          readOnly
-				></input>{" "}
-				<img
-					src="https://prasadware.vercel.app/pfp.JPG"
-					alt="zoroberry"
-					height={50}
-					width={50}
+				<div
 					style={{
-						border: "2px solid #fff",
-						borderRadius: "50%",
-            marginLeft : "5px"
+						display: "flex",
+						justifyContent: "space-between",
+						gap: "40px",
 					}}
-				/>
-				<br />
+				>
+					<WalletMultiButton />
+					<WalletDisconnectButton />
+				</div>
+				<h1>Liquid Staked Solana </h1>
+				<div className="stake-unstake-container">
+					<div className="stake-unstake-section">
+						{/* Stake Section */}
+						<div style={{ display: "flex", gap: "5px" }}>
+							<input
+								className="inputText"
+								type="text"
+								placeholder="Amount"
+								onChange={(e) => {
+									setAmount(Number(e.target.value));
+									setLSTAmount(e.target.value);
+								}}
+							/>
+							<img
+								src="https://img.icons8.com/?size=100&id=icTiMgoOHSVy&format=png&color=000000"
+								alt="solana"
+								height={60}
+								width={60}
+							/>
+						</div>
+						<br />
+						<div style={{ display: "flex", gap: "5px" }}>
+							<input
+								className="inputText"
+								type="text"
+								placeholder="ZoroBerry"
+								value={lst}
+								readOnly
+							/>
+							<img
+								src="https://prasadware.vercel.app/pfp.JPG"
+								alt="zoroberry"
+								height={50}
+								width={50}
+								style={{
+									border: "2px solid #fff",
+									borderRadius: "50%",
+									marginLeft: "5px",
+								}}
+							/>
+						</div>
+						<br />
+						<button className="btn" onClick={stakeSol}>
+							Stake SOL
+						</button>
+					</div>
+					<div className="stake-unstake-section">
+						{/* Unstake Section */}
+						<div style={{ display: "flex", gap: "5px" }}>
+							<input
+								className="inputText"
+								type="text"
+								placeholder="ZoroBerry"
+								onChange={(e) => {
+									setUnstakeAmount(Number(e.target.value));
+									setSolAmount(e.target.value);
+								}}
+							/>
+							<img
+								src="https://prasadware.vercel.app/pfp.JPG"
+								alt="zoroberry"
+								height={50}
+								width={50}
+								style={{
+									border: "2px solid #fff",
+									borderRadius: "50%",
+									marginLeft: "5px",
+								}}
+							/>
+						</div>
+						<br />
+						<div style={{ display: "flex", gap: "5px" }}>
+							<input
+								className="inputText"
+								type="text"
+								placeholder="SOL"
+								value={unstakeSol}
+								readOnly
+							/>
+
+							<br />
+							<img
+								src="https://img.icons8.com/?size=100&id=icTiMgoOHSVy&format=png&color=000000"
+								alt="solana"
+								height={60}
+								width={60}
+							/>
+						</div>
+						<br />
+						<button className="btn" onClick={withdrawSol}>
+							UnStake SOL
+						</button>
+					</div>
+				</div>
 			</div>
-			<br />
-			<button className="btn" onClick={stakeSol}>
-				Stake SOL
-			</button>
-		</div>
+		</>
 	);
 };
 
